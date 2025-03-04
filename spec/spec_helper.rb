8a21560f525a22ad92b68ddf49b237633594bd85
David Runger <davidjrunger@gmail.com>
2025-03-04 07:49:02 -0600

    Remove appsignal integration
    This essentially reverts https://github.com/linkyndy/pallets/pull/ 40 .

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pallets'
 require 'pallets/cli'
-require 'pallets/middleware/appsignal_instrumenter'
 require 'timecop'
 
 Pallets.logger.level = Logger::FATAL
